465e4d067bb82cd692d5923f5c726cc0fe9a5bde
Craig <devethancraig@gmail.com>
2022-12-20 16:02:01 -0700

    Initial Project Structure - Display Page & Styling

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import CategoryItem from "../category-item/category-item.component";
+
+import './directory.styles.scss';
+
+const Directory = ({ categories }) => {
+    return (
+        <div className="categories-container">
+            {categories.map((category) => (
+                <CategoryItem category={category} key={category.id} />
+            ))}
+        </div>
+    )
+}
+
+export default Directory;
